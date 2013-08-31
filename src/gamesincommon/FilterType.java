2a85bc6b23efc4a642b148d7fb69c56b94cc6df7
Jaime Lennox <jaimelennoxster@gmail.com>
2013-08-31 12:28:13 +0100

    Refactored enum to separate file.

---
+++ src/gamesincommon/FilterType.java
@@ -0,0 +1,34 @@
+package gamesincommon;
+
+enum FilterType {
+
+  multiplayer("Multi-player"), coop("Co-op");
+
+  private String value;
+
+  FilterType(String value) {
+    this.value = value;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  @Override
+  public String toString() {
+    return this.getValue();
+  }
+
+  public static FilterType getEnum(String value) {
+    
+    if (value == null)
+      throw new IllegalArgumentException();
+    
+    for (FilterType v : values())
+      if (value.equalsIgnoreCase(v.getValue()))
+        return v;
+    
+    throw new IllegalArgumentException();
+    
+  }
+};
