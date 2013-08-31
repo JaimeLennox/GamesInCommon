5f12649d5385906d15a247a1d752f398b9aed6e7
Shu Sam Chen <ssc2g12@soton.ac.uk>
2013-08-31 02:11:26 +0100

    Successfully finds games in common between two test SteamIDs.

--- src/gamesincommon/GamesInCommon.java
+++ src/gamesincommon/GamesInCommon.java
@@ -9,21 +9,40 @@ import com.github.koraktor.steamcondenser.steam.community.SteamGame;
 import com.github.koraktor.steamcondenser.steam.community.SteamId;
 
 public class GamesInCommon {
-	
-	private final String HSARsID = "76561197998157106";
 
 	public GamesInCommon() {
-		SteamId id;
+		// LEGACY CODE
+		// try {
+		// // get games for HSAR
+		// SteamId id = SteamId.create("HSAR");
+		// Map<Integer, SteamGame> gamesMap = id.getGames();
+		// Set<Map.Entry<Integer, SteamGame>> games = gamesMap.entrySet();
+		// // lists games for HSAR (probably bad code, just testing right now)
+		// for (Entry<Integer, SteamGame> i : games) {
+		// System.out.println(i.getValue().getName());
+		// }
+		// } catch (SteamCondenserException e) {
+		// System.err.println(e.getMessage());
+		// }
 		try {
-			id = SteamId.create("HSAR");
-			Map<Integer, SteamGame> gamesMap = id.getGames();
-			Set<Map.Entry<Integer, SteamGame>> games = gamesMap.entrySet();
-			for (Entry<Integer, SteamGame> i : games ) {
-				System.out.println(i);
+			Set<Map.Entry<Integer, SteamGame>> set1 = getGames(SteamId.create("HSAR"));
+			System.out.println("SET1 size = "+set1.size());
+			Set<Map.Entry<Integer, SteamGame>> set2 = getGames(SteamId.create("yammy24"));
+			System.out.println("SET2 size = "+set2.size());
+			set1.retainAll(set2);
+			// lists games in common
+			for (Entry<Integer, SteamGame> i : set1) {
+				System.out.println(i.getValue().getName());
 			}
 		} catch (SteamCondenserException e) {
-			System.err.println(e.getMessage());
+			e.printStackTrace();
 		}
+	}
+
+	public Set<Map.Entry<Integer, SteamGame>> getGames(SteamId sid) throws SteamCondenserException {
+		Set<Map.Entry<Integer, SteamGame>> result;
+		result = sid.getGames().entrySet();
+		return result;
 	}
 
 	public static void main(String[] args) {
