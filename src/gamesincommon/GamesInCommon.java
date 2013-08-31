6b383b741dce62793d7304f77a0616d40cb7f41a
Jaime Lennox <jaimelennoxster@gmail.com>
2013-08-31 12:26:53 +0100

    Removed unused imports, removed constructor and cleaned up filterGames code.

--- src/gamesincommon/GamesInCommon.java
+++ src/gamesincommon/GamesInCommon.java
@@ -2,9 +2,7 @@ package gamesincommon;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -14,64 +12,8 @@ import java.util.List;
 import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
 import com.github.koraktor.steamcondenser.steam.community.SteamGame;
 import com.github.koraktor.steamcondenser.steam.community.SteamId;
-import com.github.koraktor.steamcondenser.steam.community.WebApi;
 
 public class GamesInCommon {
-
-	public enum filterType {
-
-		multiplayer("Multi-player"), coop("Co-op");
-
-		private String value;
-
-		filterType(String value) {
-			this.value = value;
-		}
-
-		public String getValue() {
-			return value;
-		}
-
-		@Override
-		public String toString() {
-			return this.getValue();
-		}
-
-		public static filterType getEnum(String value) {
-			if (value == null)
-				throw new IllegalArgumentException();
-			for (filterType v : values())
-				if (value.equalsIgnoreCase(v.getValue()))
-					return v;
-			throw new IllegalArgumentException();
-		}
-	};
-
-	public GamesInCommon() {
-
-		try {
-
-			List<Collection<SteamGame>> userGames = new ArrayList<Collection<SteamGame>>();
-
-			userGames.add(getGames(SteamId.create("HSAR")));
-			userGames.add(getGames(SteamId.create("yammy24")));
-
-			Collection<SteamGame> commonGames = mergeSets(userGames);
-
-			// List user game sizes.
-			for (int i = 0; i < userGames.size(); i++) {
-				System.out.println("Set " + i + " size: " + userGames.get(i).size());
-			}
-
-			// Lists games in common.
-			for (SteamGame i : commonGames) {
-				System.out.println(i.getName());
-			}
-
-		} catch (SteamCondenserException e) {
-			e.printStackTrace();
-		}
-	}
 
 	/**
 	 * Displays all common games from the given steam users.
@@ -158,33 +100,32 @@ public class GamesInCommon {
 	 *            Collection of filters to apply.
 	 * @return A collection of games matching one or more of filters.
 	 */
-	public Collection<SteamGame> filterGames(Collection<SteamGame> gameList, List<filterType> filterList) {
-		InputStream is = null;
+	public Collection<SteamGame> filterGames(Collection<SteamGame> gameList, List<FilterType> filterList) {
+
 		Collection<SteamGame> result = new HashSet<SteamGame>();
+		
 		for (SteamGame game : gameList) {
-			try {
-				is = new URL("http://store.steampowered.com/api/appdetails/?appids=" + game.getAppId()).openStream();
-				BufferedReader br = new BufferedReader(new InputStreamReader(is));
-				// read lines in until there are no more to be read, run filter
-				// on each line looking for specified package IDs
+		  
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(
+			    new URL("http://store.steampowered.com/api/appdetails/?appids=" + game.getAppId()).openStream()));) {
+				
+				// Read lines in until there are no more to be read, run filter on each
+				// line looking for specified package IDs.
+			  
 				String line;
+				
 				while ((line = br.readLine()) != null) {
-					for (filterType filter : filterList) {
+					for (FilterType filter : filterList) {
 						if (line.contains(filter.getValue())) {
 							result.add(game);
 						}
 					}
 				}
+				
 			} catch (IOException e) {
 				e.printStackTrace();
-			} finally {
-				try {
-					if (is != null)
-						is.close();
-				} catch (IOException ioe) {
-					// nothing to see here
-				}
 			}
+			
 		}
 		return result;
 	}
