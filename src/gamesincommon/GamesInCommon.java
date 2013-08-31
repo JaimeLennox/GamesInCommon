31b2387a6df60a732cd4d503311b35924fd193c0
Shu Sam Chen <ssc2g12@soton.ac.uk>
2013-08-31 01:53:10 +0100

    Added JSON library Added Apache Commons Lang library Initial code to fetch game list

--- src/gamesincommon/GamesInCommon.java
+++ src/gamesincommon/GamesInCommon.java
@@ -1,24 +1,33 @@
 package gamesincommon;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import com.github.koraktor.steamcondenser.exceptions.SteamCondenserException;
+import com.github.koraktor.steamcondenser.steam.community.SteamGame;
 import com.github.koraktor.steamcondenser.steam.community.SteamId;
 
 public class GamesInCommon {
+	
+	private final String HSARsID = "76561197998157106";
 
 	public GamesInCommon() {
 		SteamId id;
 		try {
 			id = SteamId.create("HSAR");
-			id.getGames();
+			Map<Integer, SteamGame> gamesMap = id.getGames();
+			Set<Map.Entry<Integer, SteamGame>> games = gamesMap.entrySet();
+			for (Entry<Integer, SteamGame> i : games ) {
+				System.out.println(i);
+			}
 		} catch (SteamCondenserException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println(e.getMessage());
 		}
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GamesInCommon gic = new GamesInCommon();
 	}
 
 }
