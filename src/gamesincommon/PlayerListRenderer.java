6666b72640f8bc4c77995a8ecfac2ae4f3915264
Shu Sam Chen <research.hyperspace@gmail.com>
2013-11-15 23:04:28 +0000

    PlayerListRenderer changes the color of a player based on their status Blue for online, green for in-game.

--- src/gamesincommon/PlayerListRenderer.java
+++ src/gamesincommon/PlayerListRenderer.java
@@ -1,5 +1,6 @@
 package gamesincommon;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,25 +13,33 @@ import com.github.koraktor.steamcondenser.steam.community.SteamId;
 
 public class PlayerListRenderer extends DefaultListCellRenderer {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+    @Override
+    public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
 
-		super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
 
-		if (value instanceof SteamId) {
-			SteamId id = (SteamId) value;
-			setText(id.getNickname());
-			try {
-				ImageIcon icon = new ImageIcon(new URL(id.getAvatarIconUrl()));
-				setIcon(icon);
-			} catch (MalformedURLException e) {
-				e.printStackTrace();
-			}
-		}
+        if (value instanceof SteamId) {
+            SteamId id = (SteamId) value;
+            setText(id.getNickname());
 
-		return this;
-	}
+            if (id.isOnline()) {
+                setForeground(Color.BLUE);
+            }
+            if (id.isInGame()) {
+                setForeground(Color.GREEN);
+            }
+
+            try {
+                ImageIcon icon = new ImageIcon(new URL(id.getAvatarIconUrl()));
+                setIcon(icon);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return this;
+    }
 
 }
